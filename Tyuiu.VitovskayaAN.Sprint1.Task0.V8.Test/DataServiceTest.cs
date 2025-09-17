5aebbf105b6c23d0ead767be60de7d53fcee72ab
qqbelll <alenkav07@listru>
2025-09-17 20:23:51 +0500

    Add project files.

---
+++ Tyuiu.VitovskayaAN.Sprint1.Task0.V8.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.VitovskayaAN.Sprint1.Task0.V8.Lib;
+namespace Tyuiu.VitovskayaAN.Sprint1.Task0.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate();
+            Assert.AreEqual(1.875, res);
+        }
+    }
+}
