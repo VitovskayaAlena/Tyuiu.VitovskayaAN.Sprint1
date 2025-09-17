f23909f3a0fe514e737c5f077b6c7a74a3c827c3
qqbelll <alenkav07@listru>
2025-09-17 22:36:13 +0500

    Task0

--- Tyuiu.VitovskayaAN.Sprint1.Task0.V0.Lib/DataService.cs
+++ Tyuiu.VitovskayaAN.Sprint1.Task0.V0.Lib/DataService.cs
@@ -3,6 +3,10 @@ namespace Tyuiu.VitovskayaAN.Sprint1.Task0.V8.Lib
 {
     public class DataService : ISprint1Task0V8
     {
+        public DataService()
+        {
+        }
+
         public double Calculate()
         {
             return 15.0 / 2.0 / 4.0;
