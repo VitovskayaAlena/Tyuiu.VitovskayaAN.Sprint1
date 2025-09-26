445c6de950f6febd7421059116019a4e99261d8b
qqbelll <alenkav07@listru>
2025-09-26 21:04:48 +0500

    Add project files.

---
+++ Tyuiu.VitovskayaAN.Sprint1.Task0.V8.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.VitovskayaAN.Sprint1.Task0.V8.Lib
+{
+    public class DataService : ISprint1Task0V8
+    {
+        public double Calculate()
+        {
+            return 15.0 / 2.0 / 4.0;
+        }
+    }
+}
